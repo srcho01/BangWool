4e6e93634ad7d0fdeca861e0a8d9afe8126c3a1e
srcho01 <seorim5306@gmail.com>
2024-07-03 13:57:48 +0900

    [Start] Spring Boot initial commit

---
+++ Backend/BangWool/src/main/java/Backend/BangWool/BangWoolApplication.java
@@ -0,0 +1,13 @@
+package Backend.BangWool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangWoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangWoolApplication.class, args);
+	}
+
+}
