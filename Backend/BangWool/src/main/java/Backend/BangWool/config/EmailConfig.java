d8aa761c7cdc5b0936196af9966908fba940b740
srcho01 <seorim5306@gmail.com>
2024-07-08 15:42:03 +0900

    [Rename] Rename folder 'Config'

--- Backend/BangWool/src/main/java/Backend/BangWool/config/EmailConfig.java
+++ Backend/BangWool/src/main/java/Backend/BangWool/config/EmailConfig.java
@@ -1,4 +1,4 @@
-package Backend.BangWool.Config;
+package Backend.BangWool.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
--- Backend/BangWool/src/main/java/Backend/BangWool/config/RedisConfig.java
+++ Backend/BangWool/src/main/java/Backend/BangWool/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package Backend.BangWool.Config;
+package Backend.BangWool.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
