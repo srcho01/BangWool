722cbd19abcc2630d953087aca89c344b37e00d9
srcho01 <seorim5306@gmail.com>
2024-07-08 17:04:21 +0900

    [Fix] Remove Test

--- Backend/BangWool/src/test/java/Backend/BangWool/BangWoolApplicationTests.java
+++ Backend/BangWool/src/test/java/Backend/BangWool/BangWoolApplicationTests.java
@@ -1,13 +1,7 @@
 package Backend.BangWool;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BangWoolApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
