1e4cdcd8df487fe600e9bc477f87f801f2f3d7bd
srcho01 <seorim5306@gmail.com>
2024-07-10 14:46:59 +0900

    [Feat] Add Exception handlers

--- Backend/BangWool/src/main/java/Backend/BangWool/exception/GlobalExceptionHandler.java
+++ Backend/BangWool/src/main/java/Backend/BangWool/exception/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package Backend.BangWool.exception;
 
 import Backend.BangWool.response.StatusResponse;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.ConnectException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
-    public StatusResponse handleBadRequestHandler(BadRequestException e) {
+    public StatusResponse handleBadRequest(BadRequestException e) {
         return StatusResponse.build(400, e);
     }
 
@@ -22,13 +25,23 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public StatusResponse handleNotFoundHandler(NotFoundException e) {
+    public StatusResponse handleNotFound(NotFoundException e) {
         return StatusResponse.build(404, e);
     }
 
     @ExceptionHandler(ServerException.class)
-    public StatusResponse handleServerHandler(ServerException e) {
+    public StatusResponse handleServer(ServerException e) {
         return StatusResponse.build(500, e);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public StatusResponse handleMessageNotReadable(HttpMessageNotReadableException e) {
+        return StatusResponse.build(400, "Message format is incorrect.");
+    }
+
+    @ExceptionHandler(ConnectException.class)
+    public StatusResponse handleConnect(ConnectException e) {
+        return StatusResponse.build(500, "Access to database has been denied");
     }
 
 }
