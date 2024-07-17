9c0ea26b95064b9f9514867e2f8ae349d1830c3c
srcho01 <seorim5306@gmail.com>
2024-07-18 01:38:22 +0900

    [Feat] Add JWTFilter and LoginFilter in Spring Security

--- Backend/BangWool/src/main/java/Backend/BangWool/config/SecurityConfig.java
+++ Backend/BangWool/src/main/java/Backend/BangWool/config/SecurityConfig.java
@@ -1,16 +1,37 @@
 package Backend.BangWool.config;
 
+import Backend.BangWool.config.filter.JWTFilter;
+import Backend.BangWool.config.filter.LoginFilter;
+import Backend.BangWool.util.JWTUtil;
+import Backend.BangWool.util.RedisUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
+    private final RedisUtil redisUtil;
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        // Bean에 AuthenticationManager 등록
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -19,15 +40,29 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.formLogin(AbstractHttpConfigurer::disable); // form 로그인 방식
+        http.httpBasic(AbstractHttpConfigurer::disable); // http basic 인증 방식
+
+
+        // 경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/api-docs/**", "/swagger-ui/**").permitAll() // Swagger
-                        .requestMatchers("/v1/email/**", "/v1/signup/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 );
 
+        // filter 등록
         http
-                .csrf(AbstractHttpConfigurer::disable);
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class)
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, redisUtil), UsernamePasswordAuthenticationFilter.class);
+
+        //세션 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
