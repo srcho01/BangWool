2eb9e19f20bdeb76874ecaa86ea4d4aaf04f001c
srcho01 <seorim5306@gmail.com>
2024-07-18 01:37:54 +0900

    [Feat] JWTFilter and LoginFilter

---
+++ Backend/BangWool/src/main/java/Backend/BangWool/config/filter/JWTFilter.java
@@ -0,0 +1,103 @@
+package Backend.BangWool.config.filter;
+
+import Backend.BangWool.member.dto.SessionDto;
+import Backend.BangWool.util.CONSTANT;
+import Backend.BangWool.util.JWTUtil;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class JWTFilter extends OncePerRequestFilter {
+
+    private final JWTUtil jwtUtil;
+
+    public JWTFilter(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // 건너 뛰어야 하는 경로 건너뛰기
+        if (shouldSkip(request)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Authorization 헤더 찾기
+        String authorization = request.getHeader(CONSTANT.AUTHORIZATION_HEADER);
+
+        // Authorization 헤더 검증 (Bearer로 시작하는지 검증)
+        if (authorization == null || !authorization.startsWith(CONSTANT.BEARER_PREFIX)) {
+            System.out.println("token null");
+            setBody(response, 401, "Access token is null");
+            return;
+        }
+
+        // Bearer 접두사 제거 후 순수 토큰 획득
+        String token = authorization.split(" ")[1];
+
+        // 토큰 소멸 시간 검증
+        try {
+            if (jwtUtil.isExpired(token)) {
+                System.out.println("token expired");
+                setBody(response, 401, "Access token expired. Please reissue it.");
+                return;
+            }
+        } catch (MalformedJwtException | SignatureException | UnsupportedJwtException | IllegalArgumentException e) {
+            setBody(response, 400,"Token form is incorrect");
+            return;
+        }
+
+        // 토큰에서 정보 획득
+        String category = jwtUtil.getCategory(token);
+        int memberID = jwtUtil.getMemberID(token);
+        String username = jwtUtil.getUsername(token);
+        String role = jwtUtil.getRole(token);
+
+        // access 토큰인지 확인
+        if (!category.equals("access")) {
+            setBody(response, 400,"It's not a access token");
+            return;
+        }
+
+        // 매 요청마다 ContextHolder에 Authentication 추가 (왜냐하면 Stateless이니까)
+        SessionDto session = new SessionDto(memberID, username, role);
+        Authentication authToken = new UsernamePasswordAuthenticationToken(session, null, session.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken); // user session 생성
+
+        // 다음 필터로 넘기기
+        filterChain.doFilter(request, response);
+    }
+
+    private void setBody(HttpServletResponse response, int code, String message) throws IOException {
+        response.setStatus(code);
+        response.setContentType("application/json");
+        String jsonResponse = "{\"code\": " + code + ", \"message\": " + message + "}";
+        response.getWriter().write(jsonResponse);
+    }
+
+    private boolean shouldSkip(HttpServletRequest request) {
+        List<String> skipURI = new ArrayList<>();
+        skipURI.add("/login"); skipURI.add("/swagger-ui/.*"); skipURI.add("/api-docs/.*"); skipURI.add("/auth/refresh");
+
+        return skipURI.stream().anyMatch(uri -> {
+            Pattern pattern = Pattern.compile(uri);
+            return pattern.matcher(request.getRequestURI()).matches();
+        });
+    }
+
+}
---
+++ Backend/BangWool/src/main/java/Backend/BangWool/config/filter/LoginFilter.java
@@ -0,0 +1,112 @@
+package Backend.BangWool.config.filter;
+
+import Backend.BangWool.member.dto.CustomUserDetails;
+import Backend.BangWool.member.dto.TokenResponseDto;
+import Backend.BangWool.response.DataResponse;
+import Backend.BangWool.util.CONSTANT;
+import Backend.BangWool.util.JWTUtil;
+import Backend.BangWool.util.RedisUtil;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JWTUtil jwtUtil;
+    private final RedisUtil redisUtil;
+
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        // 클라이언트 요청에서 username, password 추출
+        String username = null;
+        String password = null;
+
+        Map<String, String> requestBody;
+        try {
+            requestBody = new ObjectMapper().readValue(request.getInputStream(), new TypeReference<Map<String, String>>(){});
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to parse JSON request body", e);
+        }
+
+        username = requestBody.get("email");
+        password = requestBody.get("password");
+
+        // 스프링 시큐리티에서 username과 pw를 검증하기 위해 token에 담기
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                username, password
+        );
+
+        // token에 담은 정보를 검증하기 위해 AuthenticationManager로 전달
+        return authenticationManager.authenticate(token);
+
+    }
+
+    // 로그인 성공 시
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+
+        // 현재 요청의 유저는 누구인가
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal(); // 사용자의 주체 반환
+
+        // 주체(principal)이란. 일반적으로 사용자 이름이나 사용자 정보를 나타내는 객체
+        String username = customUserDetails.getUsername();
+        int memberID = customUserDetails.getMemberID();
+
+        // role 알아내기
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        // token 만들기 (expiredSec : 초 단위)
+        String access = jwtUtil.generateToken("access", memberID, username, role, CONSTANT.ACCESS_EXPIRED);
+        String refresh = jwtUtil.generateToken("refresh", memberID, username, role, CONSTANT.REFRESH_EXPIRED);
+
+        // refresh 토큰 redis 저장
+        redisUtil.setDataExpire(CONSTANT.REDIS_TOKEN + refresh, "valid", CONSTANT.REFRESH_EXPIRED);
+
+
+        // 응답 구성하기 (헤더, 바디)
+        TokenResponseDto tokenDto = new TokenResponseDto(access, CONSTANT.ACCESS_EXPIRED, refresh, CONSTANT.REFRESH_EXPIRED);
+        DataResponse<TokenResponseDto> tokenBody = DataResponse.build(tokenDto);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(tokenBody);
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/json");
+        response.getWriter().write(jsonResponse);
+    }
+
+    // 로그인 실패 시
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+
+        // 응답 메시지 작성
+        String jsonResponse = "{\"code\": 401, \"message\": Unauthorized}";
+
+        // 응답에 메시지 쓰기
+        response.getWriter().write(jsonResponse);
+
+    }
+}
