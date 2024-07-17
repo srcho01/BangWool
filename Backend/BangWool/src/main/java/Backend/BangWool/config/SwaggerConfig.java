fc71dca29e6ba74e3f5c69979f21a3bc150ea6f5
srcho01 <seorim5306@gmail.com>
2024-07-18 01:34:00 +0900

    [Document] Add Authorization button

--- Backend/BangWool/src/main/java/Backend/BangWool/config/SwaggerConfig.java
+++ Backend/BangWool/src/main/java/Backend/BangWool/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package Backend.BangWool.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -16,7 +20,7 @@ public class SwaggerConfig {
 
     @Bean
     public GroupedOpenApi chatOpenApi() {
-        String[] paths = {"/v1/**"}; // 경로에 매칭되는 API를 그룹화하여 문서화
+        String[] paths = {"/**"}; // 경로에 매칭되는 API를 그룹화하여 문서화
 
         return GroupedOpenApi.builder()
                 .group("BangWool")  // 그룹 이름을 설정한다.
@@ -24,4 +28,18 @@ public class SwaggerConfig {
                 .build();
     }
 
+    @Bean
+    public OpenAPI api() {
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization")
+                .description("Description: 앞에 \"Bearer \"와 함께 /login을 실행하여 발급받은 access token을 넣어주세요");
+
+        SecurityRequirement requirement = new SecurityRequirement().addList("Bearer");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer", apiKey))
+                .addSecurityItem(requirement);
+    }
 }
