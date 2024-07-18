288d94ed47d740658596b2b5b4f38390b86a6849
srcho01 <seorim5306@gmail.com>
2024-07-18 17:42:41 +0900

    [Document] Add /logout to Swagger

--- Backend/BangWool/src/main/java/Backend/BangWool/member/controller/AuthController.java
+++ Backend/BangWool/src/main/java/Backend/BangWool/member/controller/AuthController.java
@@ -1,24 +1,25 @@
 package Backend.BangWool.member.controller;
 
 import Backend.BangWool.response.DataResponse;
+import Backend.BangWool.response.StatusResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authorization", description = "로그인, 로그아웃 등 사용자 인증 관련 API")
 @RestController
 public class AuthController {
     @Getter
-    @Setter
-    static class LoginDTO {
+    public static class LoginDTO {
         @Schema(example = "srcho01@naver.com")
         private String email;
 
@@ -26,7 +27,16 @@ public class AuthController {
         private String password;
     }
 
-    @Operation(summary = "자체 Login", tags = "Login")
+    @Getter
+    public static class LogoutDTO {
+        @Schema(example = "your_access_token")
+        private String accessToken;
+
+        @Schema(example = "your_refresh_token")
+        private String refreshToken;
+    }
+
+    @Operation(summary = "자체 Login")
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
@@ -51,5 +61,17 @@ public class AuthController {
     })
     @PostMapping("/login")
     public void login(@RequestBody LoginDTO loginDTO) {}
+
+
+    @Operation(summary = "Logout", description = "Logout 요청이 오면, 기존 Access Token과 Refresh Token이 파기됩니다")
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = StatusResponse.class)
+            )
+    )
+    @PostMapping("/logout")
+    public void logout(@RequestBody LogoutDTO logoutDTO) {}
 
 }
