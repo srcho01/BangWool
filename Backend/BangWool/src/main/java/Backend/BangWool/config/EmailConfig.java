cb28e7f98a7ca9e9b9f32f82d86ec2f301731c39
srcho01 <seorim5306@gmail.com>
2024-07-19 16:25:07 +0900

    [Refactor] Move email settings to application.properties

--- Backend/BangWool/src/main/java/Backend/BangWool/config/EmailConfig.java
+++ Backend/BangWool/src/main/java/Backend/BangWool/config/EmailConfig.java
@@ -1,4 +1,6 @@
 package Backend.BangWool.config;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -7,14 +9,27 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 @Configuration
 public class EmailConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender mailSender() {
 
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("raccoon5255@gmail.com");
-        mailSender.setPassword("huxuqjtcapngkeic");
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties javaMailProperties = new Properties();
         javaMailProperties.put("mail.transport.protocol", "smtp");
