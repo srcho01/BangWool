5d64128470a530aad3f7becf038f7666d05a3447
srcho01 <seorim5306@gmail.com>
2024-07-24 18:55:51 +0900

    [Test] Email service and controller test

---
+++ Backend/BangWool/src/test/java/Backend/BangWool/member/controller/EmailControllerTest.java
@@ -0,0 +1,169 @@
+package Backend.BangWool.member.controller;
+
+import Backend.BangWool.config.TestSecurityConfig;
+import Backend.BangWool.member.dto.EmailCheckRequest;
+import Backend.BangWool.member.dto.EmailSendRequest;
+import Backend.BangWool.member.service.EmailService;
+import Backend.BangWool.response.DataResponse;
+import Backend.BangWool.response.StatusResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.stream.Stream;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = {EmailController.class})
+@ContextConfiguration(classes = TestSecurityConfig.class)
+class EmailControllerTest {
+
+    @MockBean
+    private EmailService emailService;
+
+    @Autowired private MockMvc mvc;
+    @Autowired private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("이메일 전송 성공")
+    void mailSend() throws Exception {
+        // given
+        EmailSendRequest request = EmailSendRequest.builder()
+                .email("test@gmail.com").build();
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        // when
+        doNothing().when(emailService).sendEmail(request.getEmail());
+
+        // then
+        String responseJson = objectMapper.writeValueAsString(StatusResponse.build(200));
+        mvc.perform(post("/auth/email/send")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseJson));
+    }
+
+    private static Stream<Arguments> invalidEmailProvider() {
+        return Stream.of(
+                Arguments.of(null, "Email is required"),
+                Arguments.of("", "Email is required"),
+                Arguments.of("잘못된이메일형식", "Email is out form")
+        );
+    }
+
+    @DisplayName("이메일 전송 실패")
+    @ParameterizedTest
+    @MethodSource("invalidEmailProvider")
+    void mailSendFail(String email, String message) throws Exception {
+        // given
+        EmailSendRequest request = EmailSendRequest.builder()
+                .email(email)
+                .build();
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        // when
+        doNothing().when(emailService).sendEmail(request.getEmail());
+
+        // then
+        String responseJson = objectMapper.writeValueAsString(StatusResponse.build(400, message));
+        mvc.perform(post("/auth/email/send")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().json(responseJson));
+    }
+
+    @Test
+    @DisplayName("코드 확인 성공")
+    void mailCheckSuccess() throws Exception {
+        // given
+        EmailCheckRequest request = EmailCheckRequest.builder()
+                .email("test@gmail.com")
+                .code("TEST12")
+                .build();
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        // when
+        when(emailService.checkCode(request.getEmail(), request.getCode())).thenReturn(true);
+
+        // then
+        String responseJson = objectMapper.writeValueAsString(DataResponse.build(true));
+        System.out.println(responseJson);
+        mvc.perform(post("/auth/email/check")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseJson));
+
+    }
+
+    @Test
+    @DisplayName("다른 코드 전송")
+    void mailCheckFail() throws Exception {
+        // given
+        EmailCheckRequest request = EmailCheckRequest.builder()
+                .email("test@gmail.com")
+                .code("TEST12")
+                .build();
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        // when
+        when(emailService.checkCode(request.getEmail(), request.getCode())).thenReturn(false);
+
+        // then
+        String responseJson = objectMapper.writeValueAsString(DataResponse.build(false));
+        System.out.println(responseJson);
+        mvc.perform(post("/auth/email/check")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseJson));
+
+    }
+
+    private static Stream<Arguments> invalidCodeProvider() {
+        return Stream.of(
+                Arguments.of("잘못된이메일형식", "123456", "Email is out form"),
+                Arguments.of(null, "123456", "Email is required"),
+                Arguments.of("test@test.com", null, "Code is required")
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("코드 확인 잘못된 형식")
+    @MethodSource("invalidCodeProvider")
+    void mailCheckFail(String email, String code, String message) throws Exception {
+        // given
+        EmailCheckRequest request = EmailCheckRequest.builder()
+                .email(email)
+                .code(code)
+                .build();
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        // when
+        when(emailService.checkCode(request.getEmail(), request.getCode())).thenReturn(false);
+
+        // then
+        String responseJson = objectMapper.writeValueAsString(StatusResponse.build(400, message));
+        mvc.perform(post("/auth/email/check")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().json(responseJson));
+
+    }
+}
---
+++ Backend/BangWool/src/test/java/Backend/BangWool/member/service/EmailServiceTest.java
@@ -0,0 +1,94 @@
+package Backend.BangWool.member.service;
+
+import Backend.BangWool.util.CONSTANT;
+import Backend.BangWool.util.RedisUtil;
+import jakarta.mail.internet.MimeMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class EmailServiceTest {
+
+    @MockBean
+    private JavaMailSender mailSender;
+
+    @MockBean
+    private RedisUtil redisUtil;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    @DisplayName("이메일 보내기")
+    void sendEmail() throws Exception {
+        // given
+        String email = "test@test.com";
+
+        // mocking
+        MimeMessage mimeMessage = mock(MimeMessage.class);
+        when(mailSender.createMimeMessage()).thenReturn(mimeMessage);
+
+        // when
+        emailService.sendEmail(email);
+
+        // then - 메시지 검증
+        verify(mailSender, times(1)).send(mimeMessage);
+        verify(redisUtil, times(1)).setDataExpire(eq(CONSTANT.REDIS_EMAIL_CODE + email), anyString(), eq(600L));
+    }
+
+    @Test
+    @DisplayName("이메일 코드 확인 성공")
+    void checkCodeSuccess() {
+        // given
+        String email = "test@test.com";
+        String code = "W63F5S";
+
+        // mocking
+        when(redisUtil.getData(CONSTANT.REDIS_EMAIL_CODE + email)).thenReturn(code);
+
+        // when
+        boolean result = emailService.checkCode(email, code);
+
+        // then
+        verify(redisUtil, times(1)).getData(eq(CONSTANT.REDIS_EMAIL_CODE + email));
+        verify(redisUtil, times(1)).setDataExpire(eq(CONSTANT.REDIS_EMAIL_VERIFY + email), eq("true"), eq(1800L));
+        assertThat(result).isEqualTo(true);
+    }
+
+    private static Stream<Arguments> invalidCheckCode() {
+        return Stream.of(
+                Arguments.of("test@test.com", "TEST12", "WRONG!"),
+                Arguments.of(null, "TEST12", null),
+                Arguments.of("test@test.com", null, "GOOD!!")
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("이메일 코드 확인 실패")
+    @MethodSource("invalidCheckCode") // given
+    void checkCodeFail(String email, String code, String redisCode) {
+        // mocking
+        when(redisUtil.getData(CONSTANT.REDIS_EMAIL_CODE + email)).thenReturn(redisCode);
+
+        // when
+        boolean result = emailService.checkCode(email, code);
+
+        // then
+        verify(redisUtil, times(1)).getData(eq(CONSTANT.REDIS_EMAIL_CODE + email));
+        verify(redisUtil, never()).setDataExpire(eq(CONSTANT.REDIS_EMAIL_VERIFY + email), eq("true"), eq(1800L));
+        assertThat(result).isEqualTo(false);
+    }
+
+}
