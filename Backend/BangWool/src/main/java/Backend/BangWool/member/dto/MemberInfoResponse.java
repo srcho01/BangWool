2289950e27ce886c6fb394d12724d1846cb7cfb5
srcho01 <seorim5306@gmail.com>
2024-08-02 10:44:26 +0900

    [Document] Add Swagger annotaions

--- Backend/BangWool/src/main/java/Backend/BangWool/member/dto/MemberInfoResponse.java
+++ Backend/BangWool/src/main/java/Backend/BangWool/member/dto/MemberInfoResponse.java
@@ -1,6 +1,7 @@
 package Backend.BangWool.member.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Schema(description = "Member Information Response DTO")
 @Getter
 @Builder
 @AllArgsConstructor
@@ -15,16 +17,25 @@ import java.time.LocalDate;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class MemberInfoResponse {
 
+    @Schema(example = "1")
     private int memberID;
 
+    @Schema(example = "test@test.com")
     private String email;
 
+    @Schema(example = "name")
     private String name;
+
+    @Schema(example = "nickname")
     private String nickname;
 
+    @Schema(example = "2000-01-01")
     private LocalDate birth;
 
+    @Schema(example = "jfidksjg")
     private String googleId;
+
+    @Schema(example = "4951351")
     private String kakaoId;
 
 }
