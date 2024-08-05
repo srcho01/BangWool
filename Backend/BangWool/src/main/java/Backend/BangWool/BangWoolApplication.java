85eaf7ad42c7f67f0ac4171e898e9dffe8e88976
srcho01 <seorim5306@gmail.com>
2024-08-05 18:42:01 +0900

    [Feat] Add CORS configurer

--- Backend/BangWool/src/main/java/Backend/BangWool/BangWoolApplication.java
+++ Backend/BangWool/src/main/java/Backend/BangWool/BangWoolApplication.java
@@ -2,12 +2,27 @@ package Backend.BangWool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication()
 public class BangWoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BangWoolApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+						.addMapping("/**")
+						.allowedOrigins("*");
+			}
+		};
 	}
 
 }
