cf7a01c2db91aba0c79a5e7754c3489e57f765d7
srcho01 <seorim5306@gmail.com>
2024-08-21 00:26:49 +0900

    [Feat] Add findByMemberAndName

--- Backend/BangWool/src/main/java/Backend/BangWool/cosmetics/repository/CosmeticsRepository.java
+++ Backend/BangWool/src/main/java/Backend/BangWool/cosmetics/repository/CosmeticsRepository.java
@@ -1,7 +1,11 @@
 package Backend.BangWool.cosmetics.repository;
 
 import Backend.BangWool.cosmetics.domain.CosmeticsEntity;
+import Backend.BangWool.member.domain.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CosmeticsRepository extends JpaRepository<CosmeticsEntity, Long> {
+    Optional<CosmeticsEntity> findByMemberAndName(MemberEntity member, String name);
 }
--- Backend/BangWool/src/main/java/Backend/BangWool/cosmetics/repository/LocationRepository.java
+++ Backend/BangWool/src/main/java/Backend/BangWool/cosmetics/repository/LocationRepository.java
@@ -4,6 +4,11 @@ import Backend.BangWool.cosmetics.domain.LocationEntity;
 import Backend.BangWool.member.domain.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LocationRepository extends JpaRepository<LocationEntity, Long> {
+
     boolean existsByMemberAndName(MemberEntity member, String name);
+    Optional<LocationEntity> findByMemberAndName(MemberEntity member, String name);
+
 }
