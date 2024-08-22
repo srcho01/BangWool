925c93bd58a6e60b8e7d3c48d9c50efbee48ec77
srcho01 <seorim5306@gmail.com>
2024-08-23 01:13:47 +0900

    [Perform] Add EqualsAndHashCode annotation

--- Backend/BangWool/src/main/java/Backend/BangWool/member/dto/MemberInfoResponse.java
+++ Backend/BangWool/src/main/java/Backend/BangWool/member/dto/MemberInfoResponse.java
@@ -2,10 +2,7 @@ package Backend.BangWool.member.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.net.URI;
 import java.time.LocalDate;
@@ -16,6 +13,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@EqualsAndHashCode
 public class MemberInfoResponse {
 
     @Schema(example = "1")
