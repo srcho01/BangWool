e95bf018024ed96a869636914c00edd3249cc1d4
gyur2 <66067610+gyur2@users.noreply.github.com>
2024-09-19 00:31:18 +0900

    [Feat] add forgotPasswords page

---
+++ Frontend/BangWool/src/screens/styles/ForgotStyle.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+    backgroundColor: 'white',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+    borderRadius: 8,
+  },
+  button: {
+    backgroundColor: 'rgb(91, 115, 133)',
+    padding: 10,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default styles;
